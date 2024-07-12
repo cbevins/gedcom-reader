7d7206f65a113fbfbd140268ca2579721facc71f
Collin D Bevins <cbevins@montana.com>
2024-07-12 12:43:04 -0600

    Polished gedcom subpackages and README.md

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-export { createGedcom } from './gedcom/createGedcom.js'
+export { constructGedcom } from './gedcom/constructGedcom.js'
 export { file2JsonArray } from './file-reader/file2JsonArray.js'
 export { Gedcom } from './gedcom/Gedcom.js'
 export { intFormat } from './helpers/formatters.js'
